68ebaf2cbadaf90a113a9b1e28d9a13aa0d0d2fa
mehran feriduni <m3hr4nus@gmail.com>
2025-01-02 12:46:58 +0330

    chenge to microservice kafka geteway module

--- gateway/src/gateway.module.ts
+++ gateway/src/gateway.module.ts
@@ -1,52 +1,54 @@
 import { Module } from '@nestjs/common';
 import {  UserController } from './user.controller';
 import { GatewayService } from './gateway.service';
-import { ClientProxyFactory, RmqOptions, Transport } from '@nestjs/microservices';
+import { ClientProxyFactory, ClientsModule, RmqOptions, Transport } from '@nestjs/microservices';
 import { TaskController } from './task.controller';
 
 @Module({
-  imports: [],
+  imports: [
+    ClientsModule.register([
+      {
+        name:"USER_SERVICE",
+        transport:Transport.KAFKA,
+        options:{
+          client:{
+            clientId:"user",
+            brokers:['localhost:29092']
+          },
+          consumer:{
+            groupId:'user-consumer'
+          }
+        }
+      },
+      {
+        name:"TOKEN_SERVICE",
+        transport:Transport.KAFKA,
+        options:{
+          client:{
+            clientId:"token",
+            brokers:['localhost:29092']
+          },
+          consumer:{
+            groupId:'token-consumer'
+          }
+        }
+      },
+      {
+        name:"TASK_SERVICE",
+        transport:Transport.KAFKA,
+        options:{
+          client:{
+            clientId:"task",
+            brokers:['localhost:29092']
+          },
+          consumer:{
+            groupId:'task-consumer'
+          }
+        }
+      }
+    ])
+  ],
   controllers: [UserController,TaskController],
-  providers: [GatewayService,{
-    provide:"USER_SERVICE",
-    useFactory(){
-      return ClientProxyFactory.create({
-        transport:Transport.RMQ,
-        options:{
-         urls:["amqp://localhost:5672"],
-         queue:"user-queue",
-         queueOptions:{}
-        }
-       } as RmqOptions)
-    },
-    inject:[],
-    
-  },
-  {  provide:"TOKEN_SERVICE",
-    useFactory(){
-      return ClientProxyFactory.create({
-        transport:Transport.RMQ,
-        options:{
-         urls:["amqp://localhost:5672"],
-         queue:"token-queue",
-         queueOptions:{}
-        }
-       } as RmqOptions)
-    },
-    inject:[]},
-  {  provide:"TASK_SERVICE",
-    useFactory(){
-      return ClientProxyFactory.create({
-        transport:Transport.RMQ,
-        options:{
-         urls:["amqp://localhost:5672"],
-         queue:"task-queue",
-         queueOptions:{}
-        }
-       } as RmqOptions)
-    },
-    inject:[]}
-
-],
+  providers: [GatewayService],
 })
 export class GatewayModule {}
